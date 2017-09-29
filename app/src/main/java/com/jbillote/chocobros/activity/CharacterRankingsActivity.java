da0dd2317866fdeb56c5ba65cf50742a3cf93095
Jeremy <jbillote@gmail.com>
2017-09-28 17:21:55 -0700

    Added stub for character ranking

--- app/src/main/java/com/jbillote/chocobros/activity/CharacterRankingsActivity.java
+++ app/src/main/java/com/jbillote/chocobros/activity/CharacterRankingsActivity.java
@@ -6,6 +6,8 @@ import com.jbillote.chocobros.fflogs.model.CharacterRanking;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewStub;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 
@@ -21,5 +23,24 @@ public class CharacterRankingsActivity extends NavigationDrawerActivity {
         ViewStub stub = (ViewStub)findViewById(R.id.main_layout_stub);
         stub.setLayoutResource(R.layout.activity_character_rankings);
         stub.inflate();
+
+        String characterName = getIntent().getStringExtra(CharacterSearchActivity.EXTRA_CHARACTER_NAME);
+        String server = getIntent().getStringExtra(CharacterSearchActivity.EXTRA_CHARACTER_SERVER);
+        String region = getIntent().getStringExtra(CharacterSearchActivity.EXTRA_CHARACTER_REGION);
+
+        String[] splitName = characterName.split(" ");
+        for (int i = 0; i < splitName.length; i++) {
+            char[] temp = splitName[i].toCharArray();
+            temp[0] = Character.toUpperCase(temp[0]);
+            splitName[i] = new String(temp);
+        }
+
+        char[] temp = server.toCharArray();
+        temp[0] = Character.toUpperCase(temp[0]);
+        server = new String(temp);
+
+        String title = "Rankings for " + splitName[0] + " " + splitName[1] + " on " + server + " (" + region + ")";
+
+        ((TextView)findViewById(R.id.character_ranking_title)).setText(title);
     }
 }
